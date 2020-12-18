24d7e1f8a2304b1e663477f3559582e4c490cfdd
Dhyanil Mehta <dhyanilm2399@gmail.com>
2020-12-18 17:35:15 +0530

    Made some changes

--- NMF.py
+++ NMF.py
@@ -1,36 +1,26 @@
 import numpy as np
 import scipy.linalg as LA
 import time
-from sklearn.decomposition import non_negative_factorization, _nmf
+from sklearn.decomposition import non_negative_factorization, NMF
 
-# Randomized NMF decomposition. A = WH
-def rNMF(A, r, k):	
+# NMF decomposition. A = WH
+def et_NMF(A, r, k=0, er_out=False):	
 	# random projection
-	t_in = time.time()
-	Y = np.abs(np.dot(np.random.randn(k, np.shape(A)[0]), A))
-	t_RP = time.time() - t_in
+	# t_in = time.time()
+	# Y = np.abs(np.dot(np.random.randn(k, np.shape(A)[0]), A))
+	# t_RP = time.time() - t_in
 
 	# algorithm
-	W1, H1, _ = non_negative_factorization(Y, n_components=r)	
-	W2, _ , _  = non_negative_factorization(A, H = H1, update_H = False, n_components=r)
-	return W2, H1, t_RP
-	
-
-# Error and time of (R)NMF decomposition.
-# er_out = True calculate the error (it could take some time)
-def et_NMF(A, r, k=0, er_out=False):
-
+	# W1, H1, _ = non_negative_factorization(Y, n_components=r)
 	t_in = time.time()
-	if k == 0:
-		W, H, _ = non_negative_factorization(A, n_components=r)
-		t_RP = 0
+	nmf = NMF(n_components=r, solver='mu', init='random', max_iter=1000)
+	W = nmf.fit_transform(A)
+	H = nmf.components_
+
+	if er_out:
+		err = nmf.reconstruction_err_
 	else:
-		W, H, t_RP = rNMF(A, r, k)
-	t = time.time() - t_in
+		err = 0
 	
-	if (er_out == False):
-		error = 0
-	else:
-		error = LA.norm(A - np.matmul(W,H), 'fro')
-		
-	return W, H, error, t, t_RP
+	t = time.time() - t_in
+	return W, H, err, t
--- NMF_real.py
+++ NMF_real.py
@@ -13,34 +13,36 @@ def real_dataset(name):
 
 er_out = True
 
-name = 'salinas'
-A = real_dataset(name)
-
-rrange = np.arange(1, 20, 2)
-k = 500
-n_it = 3
-nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=0, A=A, er_out=er_out)
-nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=k, A=A, er_out=er_out)
-
-r = 10
-krange = np.asarray([10, 20, 50, 100, 200, 500, 1000])
-n_it = 3
-nmft.k_nmf(name=name, r=r, n_it=n_it, krange=krange, A=A, er_out=er_out,
-       random_proj=False)
-nmft.k_nmf(name=name, r=r, n_it=n_it, krange=krange, A=A, er_out=er_out)
-
-nmft.run_plot('k', 'nmf_'+name)
-nmft.run_plot('r', 'nmf_'+name)
-
-
-# name = 'indian_pines'
+# name = 'salinas'
 # A = real_dataset(name)
 
 # rrange = np.arange(1, 20, 2)
 # k = 500
+# salinas_endmembers = 32
 # n_it = 3
-# # nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=0, A=A, er_out=er_out)
-# # nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=k, A=A, er_out=er_out)
+# nmft.r_nmf(name=name, r=salinas_endmembers, n_it=n_it, k=0, A=A, er_out=er_out)
+# nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=k, A=A, er_out=er_out)
+
+# r = 10
+# krange = np.asarray([10, 20, 50, 100, 200, 500, 1000])
+# n_it = 3
+# nmft.k_nmf(name=name, r=r, n_it=n_it, krange=krange, A=A, er_out=er_out,
+#        random_proj=False)
+# nmft.k_nmf(name=name, r=r, n_it=n_it, krange=krange, A=A, er_out=er_out)
+
+# nmft.run_plot('k', 'nmf_'+name)
+# nmft.run_plot('r', 'nmf_'+name)
+
+
+name = 'indian_pines'
+A = real_dataset(name)
+
+# rrange = np.arange(1, 20, 2)
+# k = 500
+indian_pines_endmembers = 16
+n_it = 3
+W_list, H_list, error, total_time, num_iter = nmft.r_nmf(name=name, r=indian_pines_endmembers, n_it=n_it, k=0, A=A, er_out=er_out)
+# nmft.r_nmf(name=name, rrange=rrange, n_it=n_it, k=k, A=A, er_out=er_out)
 
 # r = 10
 # krange = np.asarray([10, 20, 50, 100, 200, 500, 1000])
@@ -51,3 +53,19 @@ nmft.run_plot('r', 'nmf_'+name)
 
 # nmft.run_plot('k', 'nmf_'+name)
 # nmft.run_plot('r', 'nmf_'+name)
+
+print("Indian Pines: \n\n")
+
+print("Original Data Cube(reshaped to 2D)(m x n x p -> m*n x p):\n")
+print(np.shape(A), "\n")
+print(A, "\n")
+
+print(f"\nUnmixed Matrices W and H for {n_it} outer iterations:\n")
+for it in range(n_it):
+    print(f"\nW_{it} Dimensions: ({np.shape(W_list[it])[0]} x {np.shape(W_list[it])[1]})\n")
+    print(W_list[it], "\n")
+
+    print(f"\nH_{it} Dimensions: ({np.shape(H_list[it])[0]} x {np.shape(H_list[it])[1]})\n")
+    print(H_list[it], "\n")
+
+print(f"\nAverage Error = {error}, Average Total Time = {total_time} s")
